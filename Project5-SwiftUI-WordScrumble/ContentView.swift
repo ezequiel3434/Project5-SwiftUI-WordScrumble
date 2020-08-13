ed6ce3865a040e8c2c96a7e6673083f38d112585
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-13 08:45:02 -0300

    Initial Commit

---
+++ Project5-SwiftUI-WordScrumble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project5-SwiftUI-WordScrumble
+//
+//  Created by Ezequiel Parada Beltran on 13/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
