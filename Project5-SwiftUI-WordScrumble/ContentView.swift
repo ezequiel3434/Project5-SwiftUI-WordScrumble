fa998fe9aeed10ddcd8f4e4d46ff6ec682242cbb
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-14 09:45:44 -0300

    Adding to a list of words

--- Project5-SwiftUI-WordScrumble/ContentView.swift
+++ Project5-SwiftUI-WordScrumble/ContentView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack{
+                TextField("Enter tour word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                List(usedWords, id: \.self){
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+        .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {
+            return
+        }
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
