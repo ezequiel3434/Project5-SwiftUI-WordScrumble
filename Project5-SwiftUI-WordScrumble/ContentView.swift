2c5580999b43e1f474b39df9407172b773581261
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-14 10:05:31 -0300

    Running code when app launches

--- Project5-SwiftUI-WordScrumble/ContentView.swift
+++ Project5-SwiftUI-WordScrumble/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
                 }
             }
         .navigationBarTitle(rootWord)
+        .onAppear(perform: startGame)
         }
     }
     
@@ -37,6 +38,16 @@ struct ContentView: View {
         }
         usedWords.insert(answer, at: 0)
         newWord = ""
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: ".txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
